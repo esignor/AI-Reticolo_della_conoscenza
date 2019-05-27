450a11997ef0bc441c3222cf56bfeb011a7e41cb
Eleonora Signor <eleonora.signor@gmail.com>
2019-05-27 18:04:00 +0200

    Risolto il problema del train, scritti gli algoritmi di previsione e train

--- rete_neurale/rete neurale/src/function.js
+++ rete_neurale/rete neurale/src/function.js
@@ -1,169 +1,61 @@
-
-//function reload() {
-    //eval($("#layerdef").val());
-
-     //enter buttons for layers
-   /*var t = '';
-    for (var i = 1; i < net.layers.length - 1; i++) { // ignore input and regression layers (first and last)
-        var butid = "button" + i;
-        t += "<input id=\"" + butid + "\" value=\"" + net.layers[i].layer_type + "(" + net.layers[i].out_depth + ")" + "\" type=\"submit\" onclick=\"updateLix(" + i + ")\" style=\"width:80px; height: 30px; margin:5px;\";>";
-    }
-   $("#layer_ixes").html(t);
-   $("#button" + lix).css('background-color', '#FFA');*/
-   //$("#cyclestatus").html('drawing neurons ' + d0 + ' and ' + d1 + ' of layer with index ' + lix + ' (' + net.layers[lix].layer_type + ')');
-//}
-
-
-//function updateLix(newlix) {
-    //$("#button" + lix).css('background-color', ''); // erase highlight
-    //lix = newlix;
-    //d0 = 0;
-    //d1 = 1; // reset these
-    //$("#button" + lix).css('background-color', '#FFA');
-
-    //$("#cyclestatus").html('drawing neurons ' + d0 + ' and ' + d1 + ' of layer with index ' + lix + ' (' + net.layers[lix].layer_type + ')');
-//}
-
-
-
-//function random_data() {
-    //data = [];
-    //labels = [];
-    //for (var k = 0; k < 40; k++) {
-       // data.push([convnetjs.randf(-3, 3), convnetjs.randf(-3, 3)]); labels.push(convnetjs.randf(0, 1) > 0.5 ? 1 : 0);
-   // }
-    //N = labels.length;
-//}
-
-
-//function circle_data() {
-    //data = [];
-    //labels = [];
-    //for (var i = 0; i < 50; i++) {
-       /* var r = convnetjs.randf(0.0, 2.0);
-        var t = convnetjs.randf(0.0, 2 * Math.PI);
-        data.push([r * Math.sin(t), r * Math.cos(t)]);
-        labels.push(1);
-    }
-    for (var i = 0; i < 50; i++) {
-        var r = convnetjs.randf(3.0, 5.0);
-        //var t = convnetjs.randf(0.0, 2*Math.PI);
-        var t = 2 * Math.PI * i / 50.0
-        data.push([r * Math.sin(t), r * Math.cos(t)]);
-        labels.push(0);
-    }
-    N = data.length;
-}*/
-
 function update() { // permette di fare il training del dato
-  
+
     console.log("inizio dell'allenamento per i dati inseriti");
+    eval($("#layerdef").val()); // mi serve per attivare train
 
     N = ArrayInput.length;
 
-    var start = new Date().getTime();
 
-    var x = new convnetjs.Vol(1, 1, 6); // parametri passati alla rete (larghezza, altezza, profondita')
-    //x.w = data[ix];
-    console.log("array "+ ArrayInput.length);
+    var x = new convnetjs.Vol(1, 1, 6); // parametri passati alla rete (larghezza, altezza, profondita'), inoltro in questo modo un punto attraverso la rete
     var avloss = 0.0; // calcolo della perdita
     for (var iters = 0; iters < 20; iters++) { // ho il dubbio che iteri solo per perfezionare l'apprendimento
         for (var ix = 0; ix < N; ix++) {
             x.w = ArrayInput[ix];
-            console.log("x "+ x + ArrayOutput[ix]);
             var stats = trainer.train(x, ArrayOutput[ix]);
             avloss += stats.loss;
         }
     }
     avloss /= N * iters;
 
-    var end = new Date().getTime();
-    var time = end - start;
 }
 
 
+function prevision() {
+    var id_prevision = document.getElementsByClassName("id_prevision");
+    var net = new convnetjs.Net();
+    net.makeLayers(layer_defs);
+    var x = new convnetjs.Vol(1, 1, 6);
+    x.w[0] = id_prevision[0].value;
 
+    if (!controlValue(x.w[0]))//controllo del contenuto della form
+        return; //inserimento non valido
 
-
-/*function draw() {
-
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-
-    var netx = new convnetjs.Vol(1, 1, 2);
-    // draw decisions in the grid
-    var density = 5.0;
-    var gridstep = 2;
-    var gridx = [];
-    var gridy = [];
-    var gridl = [];
-    for (var x = 0.0, cx = 0; x <= WIDTH; x += density, cx++) {
-        for (var y = 0.0, cy = 0; y <= HEIGHT; y += density, cy++) {
-            //var dec= svm.marginOne([(x-WIDTH/2)/ss, (y-HEIGHT/2)/ss]);
-            netx.w[0] = (x - WIDTH / 2) / ss;
-            netx.w[1] = (y - HEIGHT / 2) / ss;
-            var a = net.forward(netx, false);
-
-            if (a.w[0] > a.w[1]) ctx.fillStyle = 'rgb(175, 134, 216)';
-            else ctx.fillStyle = 'rgb(23, 214, 68)';
-
-            //ctx.fillStyle = 'rgb(150,' + Math.floor(a.w[0]*105)+150 + ',150)';
-            //ctx.fillStyle = 'rgb(' + Math.floor(a.w[0]*255) + ',' + Math.floor(a.w[1]*255) + ', 0)';
-            ctx.fillRect(x - density / 2 - 1, y - density / 2 - 1, density + 2, density + 2);
-
-            if (cx % gridstep === 0 && cy % gridstep === 0) {
-                // record the transformation information
-                var xt = net.layers[lix].out_act.w[d0]; // in screen coords
-                var yt = net.layers[lix].out_act.w[d1]; // in screen coords
-                gridx.push(xt);
-                gridy.push(yt);
-                gridl.push(a.w[0] > a.w[1]); // remember final label as well
-            }
-        }
+    var trovato = false; //controllo che i'identificativo in input sia stato inserito mediante vettore
+    for (var i = 0; i < ArrayInputSession.length & !trovato; ++i) {
+        if (ArrayInputSession[i] == x.w[0])
+            trovato = true;
+    }
+    if (!trovato) {
+        alert("id domanda non inserito in input");
+        return;
     }
 
-    // draw axes
-    ctx.beginPath();
-    ctx.strokeStyle = 'rgb(50,50,50)';
-    ctx.lineWidth = 1;
-    ctx.moveTo(0, HEIGHT / 2);
-    ctx.lineTo(WIDTH, HEIGHT / 2);
-    ctx.moveTo(WIDTH / 2, 0);
-    ctx.lineTo(WIDTH / 2, HEIGHT);
-    ctx.stroke();
-
-    // draw representation transformation axes for two neurons at some layer
-    var mmx = cnnutil.maxmin(gridx);
-    var mmy = cnnutil.maxmin(gridy);
-    visctx.clearRect(0, 0, visWIDTH, visHEIGHT);
-    visctx.strokeStyle = 'rgb(0, 0, 0)';
-    var n = Math.floor(Math.sqrt(gridx.length));
-    var ng = gridx.length;
-    var c = 0; // counter
-    visctx.beginPath()
-
-    ctx.strokeStyle = 'rgb(0,0,0)';
-    ctx.lineWidth = 1;
-    for (var i = 0; i < N; i++) {
-
-        if (labels[i] == 1) ctx.fillStyle = 'rgb(43, 155, 69)'; // colore dei pallini
-        else ctx.fillStyle = 'rgb(122, 24, 249)';
-
-        drawCircle(data[i][0] * ss + WIDTH / 2, data[i][1] * ss + HEIGHT / 2, 5.0); // per posizionare i punti
-
-    }
-}*/
-
-
-/** function abilita_trainer()
- *  metodo che ha il compito di rendere visibile all'utente il pulsante che abilita il trainer set dei dati
- */
-function abilita_trainer(){
-console.log("bottone di trainer abilitato?");
-console.log(ArrayInput[0] + ArrayOutput[0] + ArrayInput.length +  ArrayOutput.length);
-if(ArrayInput[0] != "" && ArrayOutput[0] != "" && ArrayInput.length == ArrayOutput.length){ // controllo dei dati di allenameto, devono essere inseriti e salvati negli appositi contenitori
-  document.myForm.button_trainer.style.display= "inline";
-  console.log("bottone di trainer abilitato");
+    //altrimenti inserimento id valido
+    console.log("id domanda da prevedere: " + x.w[0]);
+    var scores = net.forward(x);
+    console.log("previsione" + scores.w[0]);
 }
+
+
+function abilita_trainer() {
+    console.log(ArrayInput[0] + ArrayOutput[0] + ArrayInput.length + ArrayOutput.length);
+    if (ArrayInput[0] != "" && ArrayOutput[0] != "" && ArrayInput.length == ArrayOutput.length) { // controllo dei dati di allenameto, devono essere inseriti e salvati negli appositi contenitori
+        document.myForm.button_trainer.style.display = "inline"; // abilito i pulsanti
+        document.myForm.button_prevision.style.display = "inline";
+        document.myForm.id_question.style.display = "inline";
+        document.getElementById('id_question_label').style.display = "inline";
+        console.log("bottone di trainer abilitato");
+    }
 }
 
 
@@ -174,18 +66,22 @@ if(ArrayInput[0] != "" && ArrayOutput[0] != "" && ArrayInput.length == ArrayOutp
  *  (ArrayInput e ArrayOutput). Metodo che viene chiamato da addFields
  **/
 function formSubmit(operazione) {
+    var count = 0;
     var x = document.getElementsByClassName(operazione); // per avere il contenuto delle celle di  output
-    console.log("x"+x.length);
+    console.log("x" + x.length);
     var i;
     for (i = 0; i < x.length; i++) {
         if (operazione == "input") {
             var number = x[i].value;
             console.log("number " + number);
-            if (controlValue(number))
+            if (controlValue(number)) {
                 ArrayInput[i] = number;
+                ArrayInputSession[count] = number;
+                count = count + 1;
+            }
             else return;
         }
-        else{
+        else {
             ArrayOutput[i] = x[i].value;
         }
 
@@ -217,7 +113,7 @@ function controlValue(number) {
 function addFields() {
     // Number of inputs to create
     var number = document.getElementById("member").value;
-    if (!controlValue(number)) return;;//controllo field: deve essere stato inserito un numero
+    if (!controlValue(number)) return;//controllo field: deve essere stato inserito un numero
 
     // Container <div> where dynamic content will be placed
     var container = document.getElementById("container");
@@ -282,9 +178,40 @@ function addFields() {
     input.setAttribute('onclick', 'formSubmit("output")'); // premo save, salvo i valori di output su un array
     div.appendChild(document.createElement("br")) // quando faccio il submit i valori dei campi sono inseriti 
     div.appendChild(input); //appendo save a box_output
-    div.appendChild(document.createElement("br")) // quando faccio il submit i valori dei campi sono inseriti   
+    div.appendChild(document.createElement("br")) // quando faccio il submit i valori dei campi sono inseriti  
+
+    var input = document.createElement("input"); // bottone che attiva l'apprendimento
+    input.id = "button_trainer";
+    input.type = "submit";
+    input.value = "start trainer";
+    input.setAttribute('onclick', 'update()'); // 
+    div.appendChild(input); //appendo save a box_output
+
+    var label = document.createElement("label"); // bottone che crea la label
+    label.for = "id_question";
+    label.id = "id_question_label";
+    label.appendChild(document.createTextNode("Id domanda:"));
+    div.appendChild(label); //appendo save a box_output
+
+    var input = document.createElement("input"); // bottone che crea il box della label
+    input.name = "id_question";
+    input.id = "id_question";
+    input.type = "number";
+    input.classList.add("id_prevision");
+    div.appendChild(input); //appendo save a box_output
+
+
+    var input = document.createElement("input"); // bottone che attiva la previsione
+    input.id = "button_prevision";
+    input.type = "submit";
+    input.value = "prevision trainer";
+    input.setAttribute('onclick', 'prevision()'); // 
+    div.appendChild(input); //appendo save a box_output
 
     container.appendChild(div); // appendo box_output a container
+
+
+
 }
 
 
