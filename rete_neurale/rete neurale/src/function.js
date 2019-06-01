1b2d234b2b72c72bfb076e3f2c05b3200cea6ea8
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-01 12:46:26 +0200

    Reimpostata configurazione rete con 2 layers con 2 neuroni ciascuno

--- rete_neurale/rete neurale/src/function.js
+++ rete_neurale/rete neurale/src/function.js
@@ -7,7 +7,8 @@ var layer_defs, layer_exe, net, trainer;
 var t = "\n\
 layer_defs = [];\n\
 layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:6});\n\
-layer_defs.push({type:'fc', num_neurons:4, activation: 'tanh'});\n\
+layer_defs.push({type:'fc', num_neurons:2, activation: 'tanh'});\n\
+layer_defs.push({type:'fc', num_neurons:2, activation: 'tanh'});\n\
 layer_defs.push({type:'regression', num_neurons:6});\n\
 \n\
 net = new convnetjs.Net();\n\
