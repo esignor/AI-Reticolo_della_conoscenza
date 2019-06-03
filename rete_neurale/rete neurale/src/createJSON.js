17d9f63a9e67204f0e3cb8e4cd9d8af08747dc85
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-03 21:27:05 +0200

    Creata funzione per generale file json della rete

---
+++ rete_neurale/rete neurale/src/createJSON.js
@@ -0,0 +1,18 @@
+/** @function createJSON()
+ * metodo che tramuta la rete in formato JSON (utile per lo scanmbio dei dati), salvandola
+ * */
+
+
+
+function createJSON(){
+
+    // quando faccio la previsione 
+    // per salvare la rete in un JSON
+    var json = net.toJSON();
+    // ho salvato la rete su un JSON
+    var str = JSON.stringify(json);
+
+    layer_exe = str;
+    $("#layerexe").val(layer_exe);
+
+}
