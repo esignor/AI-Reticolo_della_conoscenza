b0ae7226a16a799d3723da2d5014a0fbd329d471
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-05 02:20:38 +0200

    Modificata rete neurale di prova  a seguto di test

--- rete_neurale/rete neurale/src/insertDati.js
+++ rete_neurale/rete neurale/src/insertDati.js
@@ -6,7 +6,7 @@ function insertDati() {
 
     layer_exe = "Inizilizzazione addestramento della rete"
     $("#layerexe").val(layer_exe);
-   for (var n = 0; n < 2000; ++n) { // genero n vettori per il training
+   /*for (var n = 0; n < 2000; ++n) { // genero n vettori per il training
 
         // generatore standard
         var ArrayInput = generator_input(6);
@@ -15,10 +15,10 @@ function insertDati() {
         layer_exe = layer_exe + "\n\Ricapitolazione dati inseriti: " + "Risposte ottenute  [" + ArrayInput + "]";
         $("#layerexe").val(layer_exe);
         update();
-    }
+    }*/
 
 
-    /*for(var n = 0; n<2000; ++n){ // genero n vettori per il training
+    for(var n = 0; n<2000; ++n){ // genero n vettori per il training
 
        // generatore con probabilita'
         var ArrayInput = generator_input_probability(6);
@@ -28,7 +28,7 @@ function insertDati() {
         layer_exe =  layer_exe + "\n\Ricapitolazione dati inseriti: " + "Risposte ottenute  [" + ArrayInput + "]"; 
         $("#layerexe").val(layer_exe);
         update();
-}*/
+}
 
     /*layer_exe = "Inizilizzazione addestramento della rete"
     $("#layerexe").val(layer_exe);
