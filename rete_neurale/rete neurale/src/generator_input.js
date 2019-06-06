bf4579a1fdeafd2ccf08cc7ddfa3d10656cf465f
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-06 21:22:18 +0200

    Corrette funzione di generazione del training set per la rete di base

--- rete_neurale/rete neurale/src/generator_input.js
+++ rete_neurale/rete neurale/src/generator_input.js
@@ -1,8 +1,8 @@
 /** @function generator_input
  *  @param dim_vettore dimensione del vettore che dovra' essere generato 
  *  @returns vector ritorna un vettore con dim_vettore di dimensione, ove per ogni cella si trova uno tra i seguenti valori:
- *  - -1: domanda non fatta -> in un numero randomico di celle.
- *  - 0:  domanda a cui il canditato ha risposto sbagliato -> se una domanda ha valore 0 significa che gli eventuali fratelli avranno valore 0 come i padri perche' di pari e superiore complessita', 
+ *  - 0: domanda non fatta -> in un numero randomico di celle.
+ *  - -1:  domanda a cui il canditato ha risposto sbagliato -> se una domanda ha valore 0 significa che gli eventuali fratelli avranno valore 0 come i padri perche' di pari e superiore complessita', 
      per i figli non puo' ancora nulla perche' di difficolta' inferiore.
     - 1: domanda a cui il candidato a risposto correttamente -> se una domanda ha valore 1 significa che tutte le eventuali domande figlie  e fratelli avranno valore 1 perche' di pari o inferiore 
     difficolta', per i padri non posso ancora dire nulla perche' di difficolta' superiore.
@@ -11,19 +11,20 @@
 
 function generator_input(dim_vettore) {
     var vector = [];
-    var valore = -1;
-    var numero_meno1 = Math.floor(Math.random() * 3); // generazione numero di -1 che devranno essere presenti nel vettore di training del dato da 0 a 2
+    var valore = 0;
+    var numero_zero = Math.floor(Math.random() * 3); // generazione numero di -1 che devranno essere presenti nel vettore di training del dato da 0 a 2
     // posizionati in vector gli 1 e gli 0
     var i = 0;
-    while (i < dim_vettore - numero_meno1) {
+    while (i < dim_vettore - numero_zero) {
         // setto se mettere a 0 o 1 la riposta
         if (valore == -1 || valore == 0)
             valore = 1;
         else
-            valore = 0;
+            valore = -1;
 
       
         var posizione_random = Math.floor(Math.random() * 6); // generazione di un numero casuale intero compreso tra 0 e 5 in qui allocare il primo valore del vettore di training
+        
         if (vector[posizione_random] == null) { // controllo che la cella non sia gia' occupata
             vector[posizione_random] = valore;
             i = i + 1;// nuova cella occupata
@@ -33,14 +34,13 @@ function generator_input(dim_vettore) {
             /**
              * Regole: se una domanda ha valore 1 significa che tutte le eventuali domande figlie  e fratelli avranno valore 1 perche' di pari o inferiore 
              * difficolta', per i padri non posso ancora dire nulla perche' di difficolta' superiore.
-             * Se una domanda ha valore 0 significa che gli eventuali fratelli avranno valore 0 come i padri perche' di pari e superiore complessita', 
+             * Se una domanda ha valore -1 significa che gli eventuali fratelli avranno valore -1 come i padri perche' di pari e superiore complessita', 
              * per i figli non puo' ancora nulla perche' di difficolta' inferiore.
              */
-
             if (posizione_random == 0) {
 
                 if (valore == 1) {
-                    if (i + numero_meno1 < dim_vettore && vector[3] == null) {
+                    if (i + numero_zero < dim_vettore && vector[3] == null) {
                         vector[3] = 1;
                         i = i + 1;
                     }
@@ -48,16 +48,16 @@ function generator_input(dim_vettore) {
 
                 else {
 
-                    if (i + numero_meno1 < dim_vettore && vector[2] == null) {
-                        vector[2] = 0;
+                    if (i + numero_zero < dim_vettore && vector[2] == null) {
+                        vector[2] = -1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[5] == null) {
-                        vector[5] = 0;
+                    if (i + numero_zero < dim_vettore && vector[5] == null) {
+                        vector[5] = -1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[3] == null) {
-                        vector[3] = 0;
+                    if (i + numero_zero < dim_vettore && vector[3] == null) {
+                        vector[3] = -1;
                         i = i + 1;
                     }
 
@@ -66,7 +66,7 @@ function generator_input(dim_vettore) {
 
 
             else if (posizione_random == 1) {
-                if (i + numero_meno1 < dim_vettore && vector[4] == null) {
+                if (i + numero_zero < dim_vettore && vector[4] == null) {
                     vector[4] = valore;
                     i = i + 1;
                 }
@@ -74,26 +74,26 @@ function generator_input(dim_vettore) {
 
 
 
-            else if (posizione_random == 2) {
+            else if (posizione_random == 2) { // posizione 3
 
                 if (valore == 1) {
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
                         vector[0] = 1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[3] == null) {
+                    if (i + numero_zero < dim_vettore && vector[3] == null) {
                         vector[3] = 1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[5] == null) {
+                    if (i + numero_zero < dim_vettore && vector[5] == null) {
                         vector[5] = 1;
                         i = i + 1;
                     }
                 }
 
                 else {
-                    if (i + numero_meno1 < dim_vettore && vector[5] == null) {
-                        vector[5] = 0;
+                    if (i + numero_zero < dim_vettore && vector[5] == null) {
+                         vector[5] = -1;
                         i = i + 1;
                     }
                 }
@@ -102,25 +102,25 @@ function generator_input(dim_vettore) {
             }
 
 
-            else if (posizione_random == 3) {
+            else if (posizione_random == 3) { 
 
                 if (valore == 1) {
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
                         vector[0] = 1;
                         i = i + 1;
                     }
                 }
                 else {
-                    if (i + numero_meno1 < dim_vettore && vector[2] == null) {
-                        vector[2] = 0;
+                    if (i + numero_zero < dim_vettore && vector[2] == null) {
+                        vector[2] = -1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[5] == null) {
-                        vector[5] = 0;
+                    if (i + numero_zero < dim_vettore && vector[5] == null) {
+                        vector[5] = -1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
-                        vector[0] = 0;
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
+                        vector[0] = -1;
                         i = i + 1;
                     }
                 }
@@ -132,7 +132,7 @@ function generator_input(dim_vettore) {
 
 
             else if (posizione_random == 4) {
-                if (i + numero_meno1 < dim_vettore && vector[1] == null) {
+                if (i + numero_zero < dim_vettore && vector[1] == null) {
                     vector[1] = valore;
                     i = i + 1;
                 }
@@ -141,25 +141,26 @@ function generator_input(dim_vettore) {
 
 
             else if (posizione_random == 5) {
+
                 if (valore == 1) {
-                    if (i + numero_meno1 < dim_vettore && vector[2] == null) {
+                    if (i + numero_zero < dim_vettore && vector[2] == null) {
                         vector[2] = 1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
                         vector[0] = 1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[3] == null) {
+                    if (i + numero_zero < dim_vettore && vector[3] == null) {
                         vector[3] = 1;
                         i = i + 1;
                     }
+                }
                     else {
-                        if (i + numero_meno1 < dim_vettore && vector[2] == null) {
-                            vector[2] = 0;
+                        if (i + numero_zero < dim_vettore && vector[2] == null) {
+                            vector[2] = -1;
                             i = i + 1;
                         }
-                    }
 
                 }
             }
@@ -167,16 +168,16 @@ function generator_input(dim_vettore) {
         }
     }
 
-    // posizionati in vector i -1
-    valore = -1;
-    for (var i = 0; i < numero_meno1; ++i) {
-        var posizioni_meno1 = Math.floor(Math.random() * 6); // individuazioni delle posizioni del vettore dove mettere -1
+    // posizionati in vector dim_vettore - numero_zero valori
+    valore = 0;
+    for (var i = 0; i < numero_zero; ++i) {
+        var posizioni_zero = Math.floor(Math.random() * 6); // individuazioni delle posizioni del vettore dove mettere 0
 
 
-        if (vector[posizioni_meno1] == null)
-            vector[posizioni_meno1] = valore;
+        if (vector[posizioni_zero] == null)
+            vector[posizioni_zero] = valore;
         else
-            i = i - 1; // la posizioni_meno1 non e' una posizione valida in quanto occupata
+            i = i - 1; // la posizioni_zero non e' una posizione valida in quanto occupata
     }
     return vector;
 
--- rete_neurale/rete neurale/src/generator_input_probability.js
+++ rete_neurale/rete neurale/src/generator_input_probability.js
@@ -6,24 +6,24 @@
 
 function generator_input_probability(dim_vettore) {
     var vector = [];
-    var valore = -1;
-    var numero_meno1 = Math.floor(Math.random() * 3); // generazione numero di -1 che devranno essere presenti nel vettore di training del dato
-    // posizionati in vector gli 1 e gli 0
+    var valore = calcolo_risposta_probabilita();
+    var numero_zero = Math.floor(Math.random() * 3); // generazione numero di 0 che devranno essere presenti nel vettore di training del dato
+    // posizionati in vector gli 1 e i -1
     var i = 0;
-    while (i < dim_vettore - numero_meno1) {
+    while (i < dim_vettore - numero_zero) {
 
 
         var posizione_random = Math.floor(Math.random() * 6); // numero della domanda estratta di indice da 0 a 5
 
-        if (vector[posizione_random] == null) { // controllo che la cella non sia gia' occupata ;
+        if (vector[posizione_random] == null) { // controllo che la cella non sia gia' occupata
             vector[posizione_random] = valore;
             i = i + 1;// nuova cella occupata
 
             // controlli sul grafo
 
             /**
-             * Regole: Per una domanda va valutata anche la domanda sorella. Se entrambe hanno lo stesso valore allora nel caso di 0 i genitori saranno settati
-             * a 0, se hanno entrambe valore 1 allora i figli saranno settati a 1. Tutto questo viene calcolato tenedo presente la probabilita' effettiva per un
+             * Regole: Per una domanda va valutata anche la domanda sorella. Se entrambe hanno lo stesso valore allora nel caso di -1 i genitori saranno settati
+             * a -1, se hanno entrambe valore 1 allora i figli saranno settati a 1. Tutto questo viene calcolato tenedo presente la probabilita' effettiva per un
              * candidato di rispondere correttamente ad una domanda P(A) = 1/3 + 1/6*s1 + 2/3*s2
              */
 
@@ -32,19 +32,19 @@ function generator_input_probability(dim_vettore) {
 
             if (posizione_random == 0) { // domanda 1
 
-                if (i + numero_meno1 < dim_vettore && vector[3] == null) { // calcolo la probabilita' che la domanda 4 sia data in modo corretto
+                if (i + numero_zero < dim_vettore && vector[3] == null) { // calcolo la probabilita' che la domanda 4 sia data in modo corretto
                     vector[3] = valore;
                     i = i + 1;
                 }
 
-                if (vector[3] == 0 && vector[0] == 0) { // sia la domanda 4 che 1 sono state date sbagliate, i padri che sono piu' complessi saranno sicuramente errati
-                    if (i + numero_meno1 < dim_vettore && vector[2] == null) {
-                        vector[2] = 0;
+                if (vector[3] == -1 && vector[0] == -1) { // sia la domanda 4 che 1 sono state date sbagliate, i padri che sono piu' complessi saranno sicuramente errati
+                    if (i + numero_zero < dim_vettore && vector[2] == null) {
+                        vector[2] = -1;
                         i = i + 1;
                     }
 
-                    if (i + numero_meno1 < dim_vettore && vector[5] == null) {
-                        vector[5] = 0;
+                    if (i + numero_zero < dim_vettore && vector[5] == null) {
+                        vector[5] = -1;
                         i = i + 1;
                     }
                 }
@@ -56,7 +56,7 @@ function generator_input_probability(dim_vettore) {
 
 
             else if (posizione_random == 1) { // domanda 2 
-                if (i + numero_meno1 < dim_vettore && vector[4] == null) {
+                if (i + numero_zero < dim_vettore && vector[4] == null) {
                     vector[4] = valore;
                     i = i + 1;
                 }
@@ -66,16 +66,16 @@ function generator_input_probability(dim_vettore) {
 
             else if (posizione_random == 2) { // domanda 3
 
-                if (i + numero_meno1 < dim_vettore && vector[5] == null) {
+                if (i + numero_zero < dim_vettore && vector[5] == null) {
                     vector[5] = valore;
                     i = i + 1;
                 }
                 if (vector[2] == 1 && vector[5] == 1) {
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
                         vector[0] = 1;
                         i = i + 1;
                     }
-                    if (i + numero_meno1 < dim_vettore && vector[3] == null) {
+                    if (i + numero_zero < dim_vettore && vector[3] == null) {
                         vector[3] = 1;
                         i = i + 1;
                     }
@@ -86,18 +86,18 @@ function generator_input_probability(dim_vettore) {
 
                 else if (posizione_random == 3) { // domanda 4
 
-                    if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                    if (i + numero_zero < dim_vettore && vector[0] == null) {
                         vector[0] = valore;  // sia che risulti che la domanda 1 viene data corretta o meno posso settare solo tale domanda
                         i = i + 1;
                     }
 
-                    if (vector[3] == 0 && vector[1] == 0) { // i genitori sono conseguentemente sbagliati altrimenti non posso dire nulla
-                        if (i + numero_meno1 < dim_vettore && vector[2] == null) {
-                            vector[2] = 0;
+                    if (vector[3] == -1 && vector[0] == -1) { // i genitori sono conseguentemente sbagliati altrimenti non posso dire nulla
+                        if (i + numero_zero < dim_vettore && vector[2] == null) {
+                            vector[2] = -1;
                             i = i + 1;
                         }
-                        if (i + numero_meno1 < dim_vettore && vector[5] == null) {
-                            vector[5] = 0;
+                        if (i + numero_zero < dim_vettore && vector[5] == null) {
+                            vector[5] = -1;
                             i = i + 1;
                         }
 
@@ -111,7 +111,7 @@ function generator_input_probability(dim_vettore) {
 
 
                 else if (posizione_random == 4) { // domanda 5
-                    if (i + numero_meno1 < dim_vettore && vector[1] == null) {
+                    if (i + numero_zero < dim_vettore && vector[1] == null) {
                         vector[1] = valore;
                         i = i + 1;
                     }
@@ -121,18 +121,18 @@ function generator_input_probability(dim_vettore) {
 
                 else if (posizione_random == 5) {
 
-                    if (i + numero_meno1 < dim_vettore && vector[2] == null) {
+                    if (i + numero_zero < dim_vettore && vector[2] == null) {
                         vector[2] = valore;
                         i = i + 1;
                     }
 
 
                     if (vector[2] == 1 && vector[5] == 1) {
-                        if (i + numero_meno1 < dim_vettore && vector[0] == null) {
+                        if (i + numero_zero < dim_vettore && vector[0] == null) {
                             vector[0] = 1;
                             i = i + 1;
                         }
-                        if (i + numero_meno1 < dim_vettore && vector[3] == null) {
+                        if (i + numero_zero < dim_vettore && vector[3] == null) {
                             vector[3] = 1;
                             i = i + 1;
                         }
@@ -144,16 +144,16 @@ function generator_input_probability(dim_vettore) {
         }
     }
 
-    // posizionati in vector i -1
-    valore = -1;
-    for (var i = 0; i < numero_meno1; ++i) {
-        var posizioni_meno1 = Math.floor(Math.random() * 6); // individuazioni delle posizioni del vettore dove mettere -1
+    // posizionati in vector dim_vettore  - numero_zero elementi
+    valore = 0;
+    for (var i = 0; i < numero_zero; ++i) {
+        var posizioni_zero = Math.floor(Math.random() * 6); // individuazioni delle posizioni del vettore dove mettere 0
 
 
-        if (vector[posizioni_meno1] == null)
-            vector[posizioni_meno1] = valore;
+        if (vector[posizioni_zero] == null)
+            vector[posizioni_zero] = valore;
         else
-            i = i - 1; // la posizioni_meno1 non e' una posizione valida in quanto occupata
+            i = i - 1; // la posizioni_zero non e' una posizione valida in quanto occupata
     }
     return vector;
 
@@ -162,8 +162,8 @@ function generator_input_probability(dim_vettore) {
 /** @function calcolo_risposta_probabilita
  *  calcola in base alla probabilita' che un candidato ha di dare la risposta corretta se un candidato sa dare una risposta o meno ad una domanda,
  *  si fa uso della formula 1/3 + 1/6 s1 + 2/3 s2
- *  @return 0 se un candidato n non sa dare la risposta, 1 altriment
- */ 
+ *  @return 0 se un candidato n non sa dare la risposta, 1 altrimenti
+ */
 
 function calcolo_risposta_probabilita() {
 
@@ -173,12 +173,20 @@ function calcolo_risposta_probabilita() {
     //valuta  la capacita' quando un candidato non sa la domanda per certo/parzialmente/non la sa proprio o  la sa
 
     var candidato_scarto1 = Math.floor(Math.random() * 2); // il candidato e' in grado di scartarne 1?
-    if (!candidato_scarto1) // non ne sa scartare nessuna, calcolo il fattore fortuna
-        return Math.floor(1 / 3 * ((Math.floor(Math.random() * 3) + 1))); // ha una possibilta' su 3 di azzeccare la risposta  -> 1/3
+    if (!candidato_scarto1) { // non ne sa scartare nessuna, calcolo il fattore fortuna
+        if (Math.floor(1 / 3 * ((Math.floor(Math.random() * 3) + 1))) == 0) // ha una possibilta' su 3 di azzeccare la risposta  -> 1/3
+            return -1;
+        else
+            return 1;
+    }
     else { // ne sa scartare una
-        var candidato_scarto2 = Math.floor(Math.random() * 3); // il candi+ dato e' in grado di scartarne 2?
-        if (!candidato_scarto2) // ne sa scartare una; ma non 2
-            return Math.floor(1 / 2 * ((Math.floor(Math.random() * 2) + 1)));// ha 1 possibilita' su 2 di saper rispondere giusto + il fattore fortuna -> 1/3 + 1/6 s1 -> s1 = 1 allora 1/3 + 1/6 = (2+1)/6 = 1/2
+        var candidato_scarto2 = Math.floor(Math.random() * 3); // il candidato e' in grado di scartarne 2?
+        if (!candidato_scarto2) { // ne sa scartare una; ma non 2
+            if (Math.floor(1 / 2 * ((Math.floor(Math.random() * 2) + 1))) == 0)// ha 1 possibilita' su 2 di saper rispondere giusto + il fattore fortuna -> 1/3 + 1/6 s1 -> s1 = 1 allora 1/3 + 1/6 = (2+1)/6 = 1/2
+                return -1
+            else
+                return 1;
+        }
         else // ne sa scartare 2
             return 1;  // -> 1/3 + 2/3 s2 -> s2 = 1 allore 1/3 + 2/3 = 3/3 =1
     }
