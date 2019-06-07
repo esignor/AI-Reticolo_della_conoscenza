85fc915bfbc1d1a9f6ea32521c6e695693b120e7
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-07 14:48:23 +0200

    Corretto bug nel metodo previsione della rete di prova

--- rete_neurale/rete neurale/src/prevision.js
+++ rete_neurale/rete neurale/src/prevision.js
@@ -10,24 +10,20 @@ function prevision() {
 
     var x = new convnetjs.Vol(1, 1, 6);
 
+
     var vettore_previsione = document.getElementsByClassName("vettore_input"); // per avere il contenuto delle celle di dati
     for (var i = 0; i < vettore_previsione.length; i++) {
         var number = vettore_previsione[i].value;
-
-
-            if (!controlValueInput(number)) //controllo che il valore inserito in input non sia vuoto
-                return;
-            x.w[i] = number; // salvo il contenuto anche nell'array necessario all'autoencoder
+        x.w[i] = number; // salvo il contenuto anche nell'array necessario al metodo forward
     }
 
-
     //previsione della rete
-    net.forward(x, false);  // chiamata al metodi di previsione, in base all'input ottengo la probabilita' di risposta;
+    var scores = net.forward(x, false);  // chiamata al metodi di previsione, in base all'input ottengo la probabilita' di risposta;
 
     layer_exe = layer_exe + "\n\Il vettore [" + x.w + "] ha previsione calcolata di [" + scores.w + "]" + "\n\Rete neurale in attesa ...";
     $("#layerexe").val(layer_exe);
 
     document.getElementById("myCanvas").style.display = "inline"; // faccio comparire il box con la previsione canvas
-    animationPrevision(scores, 6); // creo il canvas
+    animationPrevision(scores, 6); // chiamata al metodo che crea il canvas
 
 }
