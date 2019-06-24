d12f3e75a9cc92fc8308a126f532f4e8a03c328c
Eleonora Signor <eleonora.signor@gmail.com>
2019-06-24 22:19:17 +0200

    Aggiunti file CSV generati dalla matrice di correlazione

--- PCA/script/scripPCA_rete-di-prova.R
+++ PCA/script/scripPCA_rete-di-prova.R
@@ -3,35 +3,34 @@
 # Caricamento della libreria
 library("factoextra")
 options(max.print=10000)
-df<-read.table("/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/set-rete_di_prova.csv", header=FALSE, sep=",")  # carico il file csv
-
-# data.frame trasformato in una matrice numerica (per essere manipolata dal pca)
-df_numeric<-data.frame(df)
-
-
+df_numeric<-read.table("/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/set-rete_di_prova.csv", header=FALSE, sep=",")  # carico il file csv
 # calcolo il pca (centra automaticamente per avere la media a 0), non standardizzato
-res <- prcomp(df_numeric) 
-biplot(res); # rappresentazione grafica
+res.pca <- prcomp(df_numeric, scale = FALSE) 
+biplot(res.pca); # rappresentazione grafica
 
-# standardizzazione dei valori
+
+# standardizzazione dei valori: MI E' UTILE PER IL CALCOLO DEGLI AUTOVETTORI
 df_numeric_aux = df_numeric
 for(i in 1:6){
-  df_numeric_aux[,i]=(df_numeric[,i]-mean(df_numeric[,i]))/(sd(df_numeric[,i]))
+ df_numeric_aux[,i]=(df_numeric[,i]-mean(df_numeric[,i]))/(sd(df_numeric[,i]))
 }
 res.pca=princomp(df_numeric_aux)
 biplot(res.pca) # rappresentazione grafica dei risultati ottenuti dalla pca standardizzata
 
-# fornisce gli autovalori della matrice di covarianza
-summary(res.pca)
-# facendo riferimento al campo "Proportion of Variance" le prime 3 componenti sono sufficienti per a descrivere i dati (spiegano ~98% della varianza)
-# le variabili principali sono visibili anche graficamente
-std_dex<-res.pca$sdev
 
+# riepilogo dei risultati (deviazione, proporzione della varianza, proporzione cumulata)
+summary(res.pca)
+# fornisce gli autovalori della matrice di covarianza/varianza delle dimensioni principali
+get_eig(res.pca)
+
+# calcolo la varianza per rappretare nel grafico l'andamento di tutte le 6 domande con tipo double
+std_dex<-res.pca$sdev
 #calcolo della varianza
 pr_var<-std_dex^2
-
 # proporzione della varianza spiegata da ciascun componente
 prop_varex<-pr_var/sum(pr_var)
+# facendo riferimento al campo "Proportion of Variance" le prime 3 componenti sono sufficienti per a descrivere i dati (spiegano ~98% della varianza)
+# le variabili principali sono visibili anche graficamente (vedi seguenti due graici)
 plot(cumsum(prop_varex), xlab="Principal Component", ylab="Cumulative Proportion of Variance Explained", type="b")
 # ossevazione: le prime due componenti catturano buona parte della variabilita'
 # Le ultime componenti principali descrivono solo rumore (spiegano una percentuale sempre minore della variabile principale)
@@ -39,6 +38,7 @@ plot(cumsum(prop_varex), xlab="Principal Component", ylab="Cumulative Proportion
 # efficientemente utilizzato per interpretare i dati
 # estrazione dei risultati per le variabili
 fviz_screeplot(res.pca, addlabels =TRUE, barcolor = "darkblue", linecolor = "red", title = "Variances - PCA", x = "Principal Components", y = "% of variances") # equivale a plot(res.pca)
+
 # autovettori
 loadings(res.pca)
 # Risulta come V1 (prima componente principale) e' vicino a V4, V2(seconda componente principale) a V5 e V3(terza componente principale) A V6
@@ -53,50 +53,42 @@ fviz_contrib(res.pca, choice = "var", axes = 1:3, top = 10)
 
 
 
-# Grafico degli individui. Gli individui con un profilo simile sono raggruppati insieme.
-fviz_pca_ind(res.pca,
-             col.ind = "cos2", # Color by the quality of representation
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE ,    # Avoid text overlapping
-)
-# Grafico delle variabili. Le variabili correlate positive indicano lo stesso
-# lato della trama. Le variabili correlate negative puntano ai lati opposti
-# del grafico.
-fviz_pca_var(res.pca,
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
+# Grafico degli individui. Gli individui con un profilo simile sono raggruppati insieme. NON NECESSARIO PER LA MIA ANALISI
+# fviz_pca_ind(res.pca,
+#            col.ind = "cos2", # Color by the quality of representation
+#             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+#             repel = TRUE ,    # Avoid text overlapping
+#)
 
 
+  # Grafico delle variabili. Le variabili correlate positive indicano lo stesso
+  # lato della trama. Le variabili correlate negative puntano ai lati opposti
+  # del grafico.
+  fviz_pca_var(res.pca,
+               col.var = "contrib", # Color by contributions to the PC
+               gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+               repel = TRUE 
+  )
 
-cor.pca <- cor(df_numeric)# matrice di correlazione
+
+# Trasposizione dei dati in ordine decrescente di correlazione per ciascuna domanda
+cor.pca <- cor(df_numeric)
 plot(cor.pca)
-cor.pca
-
+# Trasposizione dei dati in ordine decrescente di correlazione per ciascuna domanda
 matrix <-matrix(nrow = 1, ncol = 6)
-value <- matrix(nrow = 6, ncol = 6)
-for(r in 1:6){ #passo le righe
+correlazione <- matrix(nrow = 6, ncol = 6)
+for(r in 1:6){ 
   for(k in 1:6){
     matrix[k] = cor.pca[r,k]
   }
-  matrix
   for(i in 1:6){
     index  = which.max(matrix)
-    value[r, i] = index;
+    correlazione[r, i] = index;
     matrix[index] = -1000
   }
 }
-value
+# Correlazione copiata in matrice_correlazione-prova.csv"
+write.table(correlazione, file="/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/matrice_correlazione-prova.csv", quote=T, sep=" ", dec=".", na="NA", row.name=T, col.names=T)
 
-fviz_pca_var(cor.pca,
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
 
-res.pca
-df_predict<-data.frame(V1=0, V2=0, V3=1, V4=0, V5=0, V6=0)
-df_predict
-res.predict = predict(res.pca, newdata = df_predict)
-res.predict
+  
--- PCA/script/scriptPCA_db.R
+++ PCA/script/scriptPCA_db.R
@@ -2,36 +2,30 @@
 options(max.print=10000)
 # Caricamento della libreria
 library("factoextra")
-df<-read.table("/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/set-db.csv", header=FALSE, sep=",")  # carico il file csv
-
-# data.frame trasformato in una matrice numerica (per essere manipolata dal pca)
-df_numeric<-data.matrix(df, rownames.force=NA)
-write.table(df_numeric, file="/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/prova.csv", quote=T, sep=" ", dec=".", na="NA", row.name=T, col.names=T)
-
-
+df_numeric<-read.table("/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/set-db.csv", header=FALSE, sep=",")  # carico il file csv
 # calcolo il pca (centra automaticamente per avere la media a 0), non standardizzato
-res <- prcomp(df_numeric) 
-biplot(res); # rappresentazione grafica
+res.pca1 <- prcomp(df_numeric, scale = FALSE) 
+biplot(res.pca); # rappresentazione grafica
 
-# standardizzazione dei valori
+#standardizzazione dei valori: MI E' UTILE PER CALCOLARE GLI AUTOVETTORI E APPLICARE KAISER
 df_numeric_aux = df_numeric
 for(i in 1:89){
-  df_numeric_aux[,i]=(df_numeric[,i]-mean(df_numeric[,i]))/(sd(df_numeric[,i]))
+ df_numeric_aux[,i]=(df_numeric[,i]-mean(df_numeric[,i]))/(sd(df_numeric[,i]))
 }
-
 res.pca=princomp(df_numeric_aux)
 biplot(res.pca) # rappresentazione grafica dei risultati ottenuti dalla pca standardizzata
 
-# fornisce gli autovalori della matrice di covarianza
+# riepilogo dei risultati (deviazione, proporzione della varianza, proporzione cumulata)
 summary(res.pca)
+# fornisce gli autovalori della matrice di covarianza/varianza delle dimensioni principali
 get_eig(res.pca)
 # facendo riferimento al campo "Proportion of Variance" le prime 80 componenti sono sufficienti per a descrivere i dati (spiegano ~98% della varianza)
 # le variabili principali sono visibili anche graficamente
-std_dex<-res.pca$sdev
 
+# calcolo la varianza per rappretare nel grafico l'andamento di tutte le 89 domande
+std_dex<-res.pca$sdev
 #calcolo della varianza
 pr_var<-std_dex^2
-
 # proporzione della varianza spiegata da ciascun componente
 prop_varex<-pr_var/sum(pr_var)
 plot(prop_varex, type="l")
@@ -41,7 +35,10 @@ plot(cumsum(prop_varex), xlab="Principal Component", ylab="Cumulative Proportion
 # le prime tre componenti rappresentano una percentuale sufficientemente elevata, di conseguenza il grafico che si ottiene puo' essere 
 # efficientemente utilizzato per interpretare i dati
 # estrazione dei risultati per le variabili
+# mostra l'andamento delle prime 10
 fviz_screeplot(res.pca, addlabels =TRUE, barcolor = "darkblue", linecolor = "red", title = "Variances - PCA", x = "Principal Components", y = "% of variances") # equivale a plot(res.pca)
+
+
 # autovettori
 loadings(res.pca)
 
@@ -51,48 +48,45 @@ fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
 # contributo della variabile PC2
 fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
 # contributo della variabile PC1 e PC2
-fviz_contrib(res.pca, choice = "var", axes = 1:60, top = 10)
+fviz_contrib(res.pca, choice = "var", axes = 1:41, top = 10)
 
 
 
-# Grafico degli individui. Gli individui con un profilo simile sono raggruppati insieme.
-fviz_pca_ind(res.pca,
-             col.ind = "cos2", # Color by the quality of representation
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE ,    # Avoid text overlapping
-)
+# Grafico degli individui. Gli individui con un profilo simile sono raggruppati insieme.  NON NECESSARIO PER LA MIA ANALISI
+#fviz_pca_ind(res.pca,
+#            col.ind = "cos2", # Color by the quality of representation
+#             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+#             repel = TRUE ,    # Avoid text overlapping
+#)
+
 # Grafico delle variabili. Le variabili correlate positive indicano lo stesso
 # lato della trama. Le variabili correlate negative puntano ai lati opposti
 # del grafico.
 fviz_pca_var(res.pca,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
+             repel = TRUE    
 )
 
-
-cor.pca <- cor(df_numeric_aux)# matrice di correlazione
-plot(cor.pca)
-
-matrix <-matrix(nrow = 1, ncol = 89)
-correlazione <- matrix(nrow = 89, ncol = 89)
-for(r in 1:89){ #passo le righe
-  for(k in 1:89){
-    matrix[k] = cor.pca[r,k]
+  # Calcolo della matrice di correlazione
+  cor.pca <- cor(df_numeric)
+  plot(cor.pca)
+  # Trasposizione dei dati in ordine decrescente di correlazione per ciascuna domanda
+  matrix <-matrix(nrow = 1, ncol = 89)
+  correlazione <- matrix(nrow = 89, ncol = 89)
+  for(r in 1:89){ #passo le righe
+    for(k in 1:89){
+      matrix[k] = cor.pca[r,k]
+    }
+  for(i in 1:89){
+   index  = which.max(matrix)
+   correlazione[r, i] = index;
+   matrix[index] = -1000
   }
-  matrix
-for(i in 1:89){
- index  = which.max(matrix)
- correlazione[r, i] = index;
- matrix[index] = -1000
-}
-}
-
-
-write.table(correlazione, file="/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/matrice_correlazione-domande.csv", quote=T, sep=" ", dec=".", na="NA", row.name=T, col.names=T)
-
-
-plot(correlazione)
+  }
+  # Correlazione copiata in matrice_correlazione-domande.csv"
+  write.table(correlazione, file="/home/eleonora/Scrivania/AI-Reticolo_della_conoscenza/PCA/trainset dati/matrice_correlazione-domande.csv", quote=T, sep=" ", dec=".", na="NA", row.name=T, col.names=T)
   
-  
+    
+    
   
