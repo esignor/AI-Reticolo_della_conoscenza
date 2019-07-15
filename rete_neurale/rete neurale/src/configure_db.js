02081519dbbb36ed7460ca557ebbd2de80f13e4f
SPUSTAGE02 <SPUSTAGE02@zg.local>
2019-07-15 18:04:26 +0200

    Cambiata configurazione rete 89

--- rete_neurale/rete neurale/src/configure_db.js
+++ rete_neurale/rete neurale/src/configure_db.js
@@ -26,8 +26,9 @@ configure_db89 = function () {
     var value_rete = "\n\
         layer_defs = [];\n\
         layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:89});\n\
-        layer_defs.push({type:'fc', num_neurons:12, activation: 'tanh'});\n\
-        layer_defs.push({type:'fc', num_neurons:12, activation: 'tanh'});\n\
+        layer_defs.push({type:'fc', num_neurons:2, activation: 'tanh'});\n\
+        layer_defs.push({type:'fc', num_neurons:8, activation: 'tanh'});\n\
+        layer_defs.push({type:'fc', num_neurons:14, activation: 'tanh'});\n\
         layer_defs.push({type:'regression', num_neurons:89});\n\
         \n\
         net = new convnetjs.Net();\n\
